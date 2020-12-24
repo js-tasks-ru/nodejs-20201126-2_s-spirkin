54ce251897e9261cb3b83f1d7a074dfd741a50ab
Spirkin S <s.spirkin@gmail.com>
2020-12-25 02:22:18 +0300

    Добавляет решение для 06/01

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const Koa = require('koa');
+const Chat = require('./chat');
+const chat = new Chat();
 const app = new Koa();
 
 app.use(require('koa-static')(path.join(__dirname, 'public')));
@@ -9,9 +11,23 @@ const Router = require('koa-router');
 const router = new Router();
 
 router.get('/subscribe', async (ctx, next) => {
+  const promise = new Promise((res) => {
+    chat.subscribe(res);
+  });
+  ctx.response.body = await promise;
+
+  await next();
 });
 
 router.post('/publish', async (ctx, next) => {
+  if (ctx.request.body.message) {
+    chat.publish(ctx.request.body.message);
+    ctx.response.body = 'ok';
+  } else {
+    ctx.response.body = 'empty message';
+  }
+
+  await next();
 });
 
 app.use(router.routes());
