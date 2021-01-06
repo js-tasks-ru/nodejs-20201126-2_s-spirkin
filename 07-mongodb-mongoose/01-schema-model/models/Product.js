3b11c01522e907018c3d6aed59666acb2bbfe4fd
Spirkin S <s.spirkin@gmail.com>
2021-01-07 01:47:47 +0300

    Добавляет решение для 07-01

--- 07-mongodb-mongoose/01-schema-model/models/Product.js
+++ 07-mongodb-mongoose/01-schema-model/models/Product.js
@@ -1,7 +1,31 @@
 const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
+const types = mongoose.Schema.Types;
+
 const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  subcategory: {
+    type: types.ObjectId,
+    required: true,
+  },
+  images: [String],
 });
 
 module.exports = connection.model('Product', productSchema);
