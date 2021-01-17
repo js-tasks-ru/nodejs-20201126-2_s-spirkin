20de49caf96465317376e4198d5e850f14b10708
Spirkin S <s.spirkin@gmail.com>
2021-01-18 00:50:00 +0300

    Добавляет решение для 07/02

--- 07-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -2,7 +2,7 @@ const Category = require('../models/Category');
 
 module.exports.categoryList = async function categoryList(ctx, next) {
   const categories = await Category.find({}).exec();
-  categories.map((c) => c.toJSON());
 
   ctx.body = {categories};
+  await next();
 };
